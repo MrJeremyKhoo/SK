f2ddcc3051d5c21bbe872081000b02e271115892
jeremy_khoo <jeremy_khoo@ensigninfosecurity.com>
2024-08-01 18:07:57 +0800

    trying to add jinja2 template

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import sys
 sys.path.append("pyScripts")
 from fastapi import FastAPI, Request, Form
 from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
+
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from pyScripts.main import main
+
 import subprocess
 
 app = FastAPI()
@@ -34,7 +38,7 @@ async def favicon():
 @app.post("/submit")
 def submit_form(topic: List[str] = Form(...), words: Optional[List[str]] = Form(None)):
     words = ['도시', '이름', '저', '나', '남자', '여자', '이', '그', '저', '것', '이것', '그것', '저것', '의자', '탁자', '선생님', '침대', '집', '차', '사람', '책', '컴퓨터', '나무', '소파', '중국', '일본', '문', '의사', '학생', '이다', '네', '아니', '나라', '가방', '창문', '잡지', '방', '냉장고', '개', '강아지', '고양이', '쥐', '펜', '전화기', '커피', '식당', '건물', '텔레비전', '미국', '캐나다', '호텔', '학교', '은행', '안', '위', '밑', '옆', '뒤', '앞', '여기', '있다', '있다', '음식', '케이크', '공항', '병원', '공원', '한국어', '머리', '귀', '팔', '눈', '입', '배', '버스', '배', '우리', '먹다', '가다','만나다', '닫다', '열다', '원하다', '만들다', '하다', '말하다', '이해하다', '좋아하다', '크다','작다', '새롭다', '낡다', '비싸다', '싸다', '아름답다', '뚱뚱하다', '길다', '좋다']
-    print(words)
+    print(topic)
     os.chdir("pyScripts")
     main(topic, words)
     os.chdir("..")
